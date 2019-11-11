7c48115ee40739939430ea3228af53fcc6f7a68b
Ehsan1997 <ehsan@DESKTOP-P6IUFQD.localdomain>
2019-11-11 21:33:11 +0500

    Iterative Count function instead of recursive in SortedLinkList.py

--- SortedLinkList.py
+++ SortedLinkList.py
@@ -16,15 +16,15 @@ class SortedLinkList:
       else:
         print(node.value)
       node = node.next
-  # Python won't allow def(self, self.start_node, n = 0)
+
+
   def count(self):
-    def count(node=self.start_node, n=0):
-      if node.next == None:
-        return n
-      else:
-        return count(node.next, n + 1)
-    # This is calling the inner count method
-    return count()
+    n = 0
+    node = self.start_node
+    while node.next:
+      node = node.next
+      n+=1
+    return n
     
   def insert(self, value):
     # Initial (Empty list) Case
@@ -50,3 +50,11 @@ class SortedLinkList:
       node.next = Node(value, prev=node)
       return node.next
     # Return new node, so down pointer can be assigned
+
+  def linear_search(self, value):
+    node = self.start_node
+    while node.next:
+      if node.value == value:
+        return 1
+      node = node.next
+    return 1 if node.value == value else 0
--- goal1.py
+++ goal1.py
@@ -1,5 +1,7 @@
 # Import skiplist
 from SkipList import SkipList
+from random import randint
+import time
 
 sl = SkipList()
 # sl = SortedLinkList()
@@ -7,5 +9,21 @@ arr = [10, 3, 7, 2, 91, 1]
 for a in arr:
   sl.insert(a)
 sl.print_list()
+sl = SkipList()
+for i in range(10000):
+  k = randint(0, 100000)
+  print(f'Inserting: {k}')
+  sl.insert(k)
 
-print(f"Status: {sl.search(10)}")
+print ("Done inserting!!")
+
+# sl.print_list()
+start = time.time()
+print(f"Status: {sl.search(k)}")
+end = time.time()
+print(f'Time1: {end - start}')
+
+start = time.time()
+print(f"Status: {sl.l1.ll.linear_search(k)}")
+end = time.time()
+print(f'Time2: {end - start}')
